e4531970871e91430c325bec30b39313c3129194
Sam-Burns <sam.burns.uk@gmail.com>
2015-05-17 01:02:26 +0100

    PHPUnit scaffolding

---
+++ tests/phpunit/Psr7SymfonyTest/RequestTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace SamBurns\Psr7Symfony\Test;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class RequestTest extends TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    public function testSomething()
+    {
+        $this->markTestIncomplete();
+    }
+}
