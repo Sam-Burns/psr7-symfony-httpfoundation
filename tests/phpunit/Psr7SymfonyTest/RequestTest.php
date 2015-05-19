7260e1aea5a0cde17a8d52b42818dc0b245be734
Ciaran McNulty <cmcnulty@inviqa.com>
2015-05-19 08:35:57 +0100

    Add test for immutability after instantiation

--- tests/phpunit/Psr7SymfonyTest/RequestTest.php
+++ tests/phpunit/Psr7SymfonyTest/RequestTest.php
@@ -3,6 +3,7 @@ namespace SamBurns\Psr7Symfony\Test;
 
 use PHPUnit_Framework_TestCase as TestCase;
 use SamBurns\Psr7Symfony\Request;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class RequestTest extends TestCase
 {
@@ -23,5 +24,15 @@ class RequestTest extends TestCase
             'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0',
             $result->getHeader('User-Agent')
         );
+    }
+
+    public function testImmutabilityIsMaintainedWhenInstantiated()
+    {
+        $request = new Request($symfonyRequest = new SymfonyRequest());
+
+        $symfonyRequest->headers->add(array('X-mutable' => 'true'));
+
+        $header =$request->getHeader('X-mutable');
+        $this->assertEquals($header, null);
     }
 }
